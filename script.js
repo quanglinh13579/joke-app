4732bfa0f40e9a119227fcf3ea7e9ed195f8ec75
quanglinh13579 <nguyenquanglinh71920@gmail.com>
2025-08-03 21:04:47 +0700

    chỉnh màu like và dislike trên file style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+const jokes = [
+  `A child asked his father, "How were people born?" The father said, "Adam and Eve made babies, and their babies made more babies."  
+The child asked his mom, and she said, "We were monkeys and evolved."  
+He ran back to dad: "You lied!"  
+Dad said, "No, your mom was talking about her side of the family."`,
+
+  `Teacher: "What does the chicken give you?"  
+Student: "Eggs!"  
+Teacher: "Good. What about the pig?"  
+Student: "Bacon!"  
+Teacher: "Great. And the cow?"  
+Student: "Homework!"`,
+
+  `A housewife, an accountant, and a lawyer were asked "What's 2 + 2?"  
+The housewife said "4",  
+The accountant said "Probably 3 or 4",  
+The lawyer said "How much do you want it to be?"`
+];
